11b48bab96fbd90ae889d6769455d97c4487e4db
fakhrihabb <fakhrihabb@gmail.com>
2025-09-20 19:57:52 +0700

    ci/cd: updated settings.py

--- narrapro/settings.py
+++ narrapro/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t!da#x47!qeo%09c%&cse1q(6#g)@ns40127@%xx6r7#15rl&$'
+SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-t!da#x47!qeo%09c%&cse1q(6#g)@ns40127@%xx6r7#15rl&$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.getenv("PRODUCTION") == "true":
